cc87a7acb093a663ed5acdfa4d61e60dab8b1a14
JEREMIA-SUNNY <you@example.com>
2024-04-25 12:25:14 +0530

    change scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#3943b4] items-center justify-between p-">
-      <img alt="" src={"/one.png"}  />
+    <main className="flex h-[100vh] justify-center bg-[#3943b4]   p-">
+      <img alt="" src={"/one.png"} />
     </main>
   );
 }

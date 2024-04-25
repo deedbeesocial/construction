aff5cb49ec7a127ff0690ef38ca0ba61b784ebd2
JEREMIA-SUNNY <you@example.com>
2024-04-25 14:17:47 +0530

    NEW TITLE

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "EMI",
+  title: "Everest Mechanical Industries",
   description: "",
 };
 

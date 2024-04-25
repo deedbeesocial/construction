8eaa65d6741daa3f2865774b02e38c9716591830
JEREMIA-SUNNY <you@example.com>
2024-04-25 14:15:08 +0530

    NEW CHANGES

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Nirnaitech",
-  description: "https://www.nirnaitech.ai/ ",
+  title: "EMI",
+  description: "",
 };
 
 export default function RootLayout({

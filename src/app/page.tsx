767daa6b012ae59fdcd7c8f54ae6a22766eb9564
JEREMIA-SUNNY <you@example.com>
2024-04-26 09:59:27 +0530

    new image added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex h-[100vh] justify-center bg-[#3943b4]   p-">
-      <img alt="" src={"/one.png"} />
+    <main className="flex h-[100vh] justify-center p-8 bg-[#3943b4]   p-">
+      <img alt="" src={"/two.png"} />
     </main>
   );
 }

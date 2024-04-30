208ff92d267b802303d270cb4faa69938bac1dfe
JEREMIA-SUNNY <you@example.com>
2024-04-30 23:19:10 +0530

    mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Countdown } from "./CountDown";
+import { CountdownTwo } from "./CountDownTwo";
 
 export default function Home() {
   // const today = new Date(); // Get the current date
@@ -17,19 +18,28 @@ export default function Home() {
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
       }}
-      className="flex flex-col h-[100vh] font-Anta justify-center text-white   gap-8 items-center p-8 bg-[#3943b4]   p-"
+      className="flex flex-col min-h-screen md:h-[100vh] font-Anta md:justify-center text-white   gap-8 items-center md:pt-0 pt-8 md:p-8 bg-[#3943b4]   p-4"
     >
-      <div className="text-3xl leading-10 font-Anta text-center font-bold">
+      <div className=" text-xl md:text-3xl leading-10 font-Anta text-center font-bold">
         Thank you for visiting. <br /> This website is under construction.
       </div>
       <div>
         {" "}
-        <Countdown date={juneDate} />
+        <div className="md:flex hidden ">
+          {" "}
+          <Countdown date={juneDate} />
+        </div>
+        <div className="md:hidden flex">
+          <CountdownTwo date={juneDate} />
+        </div>
       </div>
 
-      <div className="font-Anta text-xl text-center">
+      <div className="font-Anta text-base md:text-xl text-center">
         Meanwhile, to know more about our services, <br /> please reach us at
-        +996 55 866 5998 or trancil@emi-saudi.com. <br /> Everest Mechanical Industries
+        +966 55 866 5998 or trancil@emi-saudi.com. <br /> <br />
+        <span className="text-xl md:text-3xl text-center leading-10 font-bold  font-Anta">
+          Everest Mechanical Industries International Co.
+        </span>
       </div>
     </main>
   );
